6f95bc86d62ad2e18d94340c96e33450cbf596f0
Your Name <you@example.com>
2019-02-15 11:49:06 +0530

    Golang program that takes two numbers from the command line, adds them up

---
+++ add.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+a:=0.0
+b:=0.0
+fmt.Println("Please Enter first Number:")
+fmt.Scanln(&a)
+fmt.Println("Please Enter Second Number:")
+fmt.Scanln(&b)
+c:=a+b
+fmt.Println("Addition of Two Number :",c)
+}
